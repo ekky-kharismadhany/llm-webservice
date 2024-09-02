948ecb7440638b17c4f03d5f7c2866ed7da44fe0
Ekky Kharismadhany <ekharisma@outlook.com>
2024-09-02 14:57:58 +0700

    feature/cleaning-web-scrapt

--- db/initial-table.sql
+++ db/initial-table.sql
@@ -14,3 +14,12 @@ CREATE TABLE IF NOT EXISTS source_types (
 ALTER TABLE documents ADD CONSTRAINT "fk_source_types" FOREIGN KEY ("source_type_id") REFERENCES "source_types"("id");
 
 ALTER TABLE documents ADD CONSTRAINT "unique_source" UNIQUE source;
+
+CREATE TABLE IF NOT EXISTS places (
+    uuid bigserial PRIMARY KEY,
+    name varchar(256),
+    latitude float,
+    longitude float,
+    city varchar(128),
+    province varchar(128)
+)
